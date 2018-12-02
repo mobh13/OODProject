583778ff18467686d6235ee98d376fee71f62b49
Abdulla Khalil Ebrahim Isa Ali <201601016@student.polytechnic.bh>
2018-12-03 00:44:11 +0300

    control panel form

--- OOD_Forms_Application/OOD_Forms_Application/Form1.Designer.cs
+++ OOD_Forms_Application/OOD_Forms_Application/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OOD_Forms_Application
 {
-    partial class Form1
+    partial class ControlPanel
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControlPanel));
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("View");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Search");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Add");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Edit");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Delete");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Profile", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("View");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Search");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Add");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Edit");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Delete");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Sell & Buy");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Property", new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8,
+            treeNode9,
+            treeNode10,
+            treeNode11,
+            treeNode12});
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("View");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Search");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Add");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Edit");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Delete");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Location", new System.Windows.Forms.TreeNode[] {
+            treeNode14,
+            treeNode15,
+            treeNode16,
+            treeNode17,
+            treeNode18});
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Navy;
+            this.lblTitle.Font = new System.Drawing.Font("Calisto MT", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTitle.Location = new System.Drawing.Point(723, 192);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(358, 65);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Control Panel";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(2, -34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1096, 291);
+            this.label1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.treeView1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(12, 263);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node3";
+            treeNode1.Text = "View";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "Search";
+            treeNode3.Name = "Node5";
+            treeNode3.Text = "Add";
+            treeNode4.Name = "Node6";
+            treeNode4.Text = "Edit";
+            treeNode5.Name = "Node7";
+            treeNode5.Text = "Delete";
+            treeNode6.Name = "Node0";
+            treeNode6.Text = "Profile";
+            treeNode7.Name = "Node8";
+            treeNode7.Text = "View";
+            treeNode8.Name = "Node9";
+            treeNode8.Text = "Search";
+            treeNode9.Name = "Node10";
+            treeNode9.Text = "Add";
+            treeNode10.Name = "Node11";
+            treeNode10.Text = "Edit";
+            treeNode11.Name = "Node12";
+            treeNode11.Text = "Delete";
+            treeNode12.Name = "Node18";
+            treeNode12.Text = "Sell & Buy";
+            treeNode13.Name = "Node1";
+            treeNode13.Text = "Property";
+            treeNode14.Name = "Node13";
+            treeNode14.Text = "View";
+            treeNode15.Name = "Node14";
+            treeNode15.Text = "Search";
+            treeNode16.Name = "Node15";
+            treeNode16.Text = "Add";
+            treeNode17.Name = "Node16";
+            treeNode17.Text = "Edit";
+            treeNode18.Name = "Node17";
+            treeNode18.Text = "Delete";
+            treeNode19.Name = "Node2";
+            treeNode19.Text = "Location";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode13,
+            treeNode19});
+            this.treeView1.Size = new System.Drawing.Size(200, 424);
+            this.treeView1.TabIndex = 4;
+            // 
+            // ControlPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1093, 694);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "ControlPanel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control Panel";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- OOD_Forms_Application/OOD_Forms_Application/Form1.cs
+++ OOD_Forms_Application/OOD_Forms_Application/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace OOD_Forms_Application
 {
-    public partial class Form1 : Form
+    public partial class ControlPanel : Form
     {
-        public Form1()
+        public ControlPanel()
         {
             InitializeComponent();
         }
