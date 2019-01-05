d0871660b400c7120d5eead9eee3106515fa2c13
AA <AA@DESKTOP-F9HCD6F>
2019-01-05 23:41:26 +0300

    Update LocationEdit.Designer.cs

--- OODNew/OODNew/LocationEdit.Designer.cs
+++ OODNew/OODNew/LocationEdit.Designer.cs
@@ -52,10 +52,9 @@
             this.lblTitle.BackColor = System.Drawing.Color.Navy;
             this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.lblTitle.Location = new System.Drawing.Point(458, 118);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Location = new System.Drawing.Point(714, 172);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(265, 45);
+            this.lblTitle.Size = new System.Drawing.Size(380, 69);
             this.lblTitle.TabIndex = 3;
             this.lblTitle.Text = "Edit Location";
             this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
@@ -66,10 +65,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.BackColor = System.Drawing.Color.Navy;
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(-1, -1);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(-2, -1);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(720, 166);
+            this.label1.Size = new System.Drawing.Size(1096, 243);
             this.label1.TabIndex = 4;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
@@ -78,11 +76,9 @@
             this.lblLocationID.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblLocationID.AutoSize = true;
             this.lblLocationID.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLocationID.Location = new System.Drawing.Point(175, 190);
-            this.lblLocationID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
+            this.lblLocationID.Location = new System.Drawing.Point(242, 291);
             this.lblLocationID.Name = "lblLocationID";
-            this.lblLocationID.Size = new System.Drawing.Size(119, 22);
+            this.lblLocationID.Size = new System.Drawing.Size(177, 34);
             this.lblLocationID.TabIndex = 6;
             this.lblLocationID.Text = "Location ID:";
             // 
@@ -92,26 +88,19 @@
             this.cmbLocations.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cmbLocations.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbLocations.FormattingEnabled = true;
-
-            this.cmbLocations.Location = new System.Drawing.Point(297, 187);
-            this.cmbLocations.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-
+            this.cmbLocations.Location = new System.Drawing.Point(477, 288);
             this.cmbLocations.Name = "cmbLocations";
-            this.cmbLocations.Size = new System.Drawing.Size(200, 30);
+            this.cmbLocations.Size = new System.Drawing.Size(298, 42);
             this.cmbLocations.TabIndex = 7;
-
-
             this.cmbLocations.SelectionChangeCommitted += new System.EventHandler(this.cmbLocations_SelectionChangeCommitted);
-
             // 
             // lblLine
             // 
             this.lblLine.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblLine.Location = new System.Drawing.Point(25, 231);
-            this.lblLine.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLine.Location = new System.Drawing.Point(101, 345);
             this.lblLine.Name = "lblLine";
-            this.lblLine.Size = new System.Drawing.Size(626, 1);
+            this.lblLine.Size = new System.Drawing.Size(939, 1);
             this.lblLine.TabIndex = 9;
             // 
             // btnClear
@@ -119,11 +108,9 @@
             this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnClear.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClear.ForeColor = System.Drawing.Color.Red;
-            this.btnClear.Location = new System.Drawing.Point(336, 387);
-            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClear.Location = new System.Drawing.Point(448, 553);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(87, 27);
-
+            this.btnClear.Size = new System.Drawing.Size(162, 39);
             this.btnClear.TabIndex = 30;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -133,11 +120,9 @@
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnClose.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(445, 387);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClose.Location = new System.Drawing.Point(662, 553);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(87, 27);
-
+            this.btnClose.Size = new System.Drawing.Size(162, 39);
             this.btnClose.TabIndex = 31;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -147,27 +132,21 @@
             this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.Color.Black;
-            this.btnUpdate.Location = new System.Drawing.Point(229, 387);
-            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUpdate.Location = new System.Drawing.Point(240, 553);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(87, 27);
-
+            this.btnUpdate.Size = new System.Drawing.Size(162, 39);
             this.btnUpdate.TabIndex = 32;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
-
             // 
             // txtLocationName
             // 
             this.txtLocationName.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtLocationName.Font = new System.Drawing.Font("Century Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-           this.txtLocationName.Location = new System.Drawing.Point(297, 257);
-            this.txtLocationName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-
-
+            this.txtLocationName.Location = new System.Drawing.Point(477, 386);
             this.txtLocationName.Name = "txtLocationName";
-            this.txtLocationName.Size = new System.Drawing.Size(200, 29);
+            this.txtLocationName.Size = new System.Drawing.Size(298, 39);
             this.txtLocationName.TabIndex = 37;
             // 
             // lblSubID
@@ -175,12 +154,9 @@
             this.lblSubID.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblSubID.AutoSize = true;
             this.lblSubID.Font = new System.Drawing.Font("Century Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-
-            this.lblSubID.Location = new System.Drawing.Point(141, 316);
-            this.lblSubID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
+            this.lblSubID.Location = new System.Drawing.Point(242, 469);
             this.lblSubID.Name = "lblSubID";
-            this.lblSubID.Size = new System.Drawing.Size(158, 21);
+            this.lblSubID.Size = new System.Drawing.Size(228, 32);
             this.lblSubID.TabIndex = 36;
             this.lblSubID.Text = "Location Sup-ID: ";
             // 
@@ -189,12 +165,9 @@
             this.lblLocationName.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblLocationName.AutoSize = true;
             this.lblLocationName.Font = new System.Drawing.Font("Century Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-
-            this.lblLocationName.Location = new System.Drawing.Point(147, 259);
-            this.lblLocationName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
+            this.lblLocationName.Location = new System.Drawing.Point(242, 389);
             this.lblLocationName.Name = "lblLocationName";
-            this.lblLocationName.Size = new System.Drawing.Size(151, 21);
+            this.lblLocationName.Size = new System.Drawing.Size(218, 32);
             this.lblLocationName.TabIndex = 35;
             this.lblLocationName.Text = "Location Name:";
             // 
@@ -203,10 +176,9 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Navy;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(2, -2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(342, 166);
+            this.pictureBox1.Size = new System.Drawing.Size(513, 243);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -216,21 +188,19 @@
             // 
             this.txtSubID.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtSubID.Font = new System.Drawing.Font("Century Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSubID.Location = new System.Drawing.Point(486, 458);
+            this.txtSubID.Location = new System.Drawing.Point(477, 466);
+            this.txtSubID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSubID.Name = "txtSubID";
             this.txtSubID.Size = new System.Drawing.Size(298, 39);
             this.txtSubID.TabIndex = 38;
             // 
             // LocationEdit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(719, 445);
-
-            this.ClientSize = new System.Drawing.Size(1079, 650);
+            this.ClientSize = new System.Drawing.Size(1090, 637);
             this.Controls.Add(this.txtSubID);
-
             this.Controls.Add(this.txtLocationName);
             this.Controls.Add(this.lblSubID);
             this.Controls.Add(this.lblLocationName);
@@ -243,7 +213,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LocationEdit";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Edit Location";
